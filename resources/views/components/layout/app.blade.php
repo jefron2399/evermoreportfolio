3a867273e222b9bd56c03aae5f1b743df2921d55
Jefron Pierce <evermoremwase01@gmail.com>
2025-05-08 01:58:37 +0200

    Vite css

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Evermore's Portfolio</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
 <body class="bg-gray-900 text-white">
