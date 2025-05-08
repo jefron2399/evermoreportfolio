b748280a855aacfeec27db30cf7daa424c94695d
Jefron Pierce <evermoremwase01@gmail.com>
2025-05-08 04:20:11 +0200

    some updates

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Evermore's Portfolio</title>
+    <title>Welcome</title>
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 
     {{-- <link rel="stylesheet" href="{{ asset('css/custom.css') }}"> --}}
--- resources/views/livewire/portfolio.blade.php
+++ resources/views/livewire/portfolio.blade.php
@@ -11,7 +11,7 @@
 <!-- Content -->
 <div class="relative z-10 flex flex-col items-center">
    <img src="{{ secure_asset('images/profile.jpeg') }}" alt="Evermore" class="w-72 h-72 rounded-full border-4 border-gray-700 shadow-lg">
-   <h2 class="text-5xl font-bold mt-6 text-white">Evermore Harmony Mwase</h2>
+   <h2 class="text-5xl font-bold mt-6 text-white">Evermore Mwase</h2>
    <p class="text-xl mt-4 text-gray-300">BSc Computer Engineering | Web & Software Developer</p>
    <a href="#projects" class="mt-8 px-6 py-3 bg-blue-600 rounded-lg hover:bg-blue-500">View My Work</a>
 </div>
