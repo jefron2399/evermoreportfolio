d5e8feac1cc47a851706e0d32bf0a362b1cf7176
Jefron Pierce <evermoremwase01@gmail.com>
2025-05-08 02:25:56 +0200

    custom style

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -6,7 +6,9 @@
     <title>Evermore's Portfolio</title>
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/custom.css') }}"> --}}
+
+    <link rel="stylesheet" href="{{ secure_asset('css/custom.css') }}">
     @livewireStyles
 </head>
 <body class="bg-gray-900 text-white">
